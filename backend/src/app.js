ed170c6eb93338386a09db1e8f379382a1801960
Pushan Mitra <pushan.mitra1@ca.ey.com>
2023-08-25 15:14:45 -0700

    IOSAS | CI/CD add new logic for build promotion

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,35 +39,37 @@ app.use(noCache());
 }));*/
 //tells the app to use json as means of transporting data
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '50mb'
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: '50mb',
+  })
+);
 
 const logStream = {
   write: (message) => {
     log.info(message);
-  }
+  },
 };
-
-
 
 const cookie = {
   secure: true,
   httpOnly: true,
-  maxAge: 1800000 //30 minutes in ms. this is same as session time. DO NOT MODIFY, IF MODIFIED, MAKE SURE SAME AS SESSION TIME OUT VALUE.
+  maxAge: 1800000, //30 minutes in ms. this is same as session time. DO NOT MODIFY, IF MODIFIED, MAKE SURE SAME AS SESSION TIME OUT VALUE.
 };
 if ('local' === config.get('environment')) {
   cookie.secure = false;
 }
 //sets cookies for security purposes (prevent cookie access, allow secure connections only, etc)
-app.use(session({
-  name: 'iosas_cookie',
-  secret: config.get('oidc:clientSecret'),
-  resave: false,
-  saveUninitialized: true,
-  cookie: cookie,
-}));
+app.use(
+  session({
+    name: 'iosas_cookie',
+    secret: config.get('oidc:clientSecret'),
+    resave: false,
+    saveUninitialized: true,
+    cookie: cookie,
+  })
+);
 healthCheck.configure();
 app.use(healthCheck.router);
 app.use(require('./routes/version').router);
@@ -75,29 +77,52 @@ app.use(require('./routes/version').router);
 app.use(passport.initialize());
 app.use(passport.session());
 
-function addLoginPassportUse(discovery, strategyName, callbackURI, kc_idp_hint) {
-  passport.use(strategyName, new OidcStrategy({
-    issuer: discovery.issuer,
-    authorizationURL: discovery.authorization_endpoint,
-    tokenURL: discovery.token_endpoint,
-    userInfoURL: discovery.userinfo_endpoint,
-    clientID: config.get('oidc:clientId'),
-    clientSecret: config.get('oidc:clientSecret'),
-    callbackURL: callbackURI,
-    scope: discovery.scopes_supported,
-    kc_idp_hint: kc_idp_hint
-  }, (_issuer, profile, _context, _idToken, accessToken, refreshToken, done) => {
-    if ((typeof (accessToken) === 'undefined') || (accessToken === null) ||
-      (typeof (refreshToken) === 'undefined') || (refreshToken === null)) {
-      return done('No access token', null);
-    }
-    //set access and refresh tokens
-    profile.jwtFrontend = auth.generateUiToken();
-    profile.jwt = accessToken;
-    profile._json = parseJwt(accessToken);
-    profile.refreshToken = refreshToken;
-    return done(null, profile);
-  }));
+function addLoginPassportUse(
+  discovery,
+  strategyName,
+  callbackURI,
+  kc_idp_hint
+) {
+  passport.use(
+    strategyName,
+    new OidcStrategy(
+      {
+        issuer: discovery.issuer,
+        authorizationURL: discovery.authorization_endpoint,
+        tokenURL: discovery.token_endpoint,
+        userInfoURL: discovery.userinfo_endpoint,
+        clientID: config.get('oidc:clientId'),
+        clientSecret: config.get('oidc:clientSecret'),
+        callbackURL: callbackURI,
+        scope: discovery.scopes_supported,
+        kc_idp_hint: kc_idp_hint,
+      },
+      (
+        _issuer,
+        profile,
+        _context,
+        _idToken,
+        accessToken,
+        refreshToken,
+        done
+      ) => {
+        if (
+          typeof accessToken === 'undefined' ||
+          accessToken === null ||
+          typeof refreshToken === 'undefined' ||
+          refreshToken === null
+        ) {
+          return done('No access token', null);
+        }
+        //set access and refresh tokens
+        profile.jwtFrontend = auth.generateUiToken();
+        profile.jwt = accessToken;
+        profile._json = parseJwt(accessToken);
+        profile.refreshToken = refreshToken;
+        return done(null, profile);
+      }
+    )
+  );
 }
 
 const parseJwt = (token) => {
@@ -109,44 +134,64 @@ const parseJwt = (token) => {
 };
 
 //initialize our authentication strategy
-utils.getOidcDiscovery().then(discovery => {
+utils.getOidcDiscovery().then((discovery) => {
   //OIDC Strategy is used for authorization
-  addLoginPassportUse(discovery, 'oidcBceid', config.get('server:frontend') + '/api/auth/callback_bceid', 'basic-bceid');
-  addLoginPassportUse(discovery, 'oidcBceidActivateUser', config.get('server:frontend') + '/api/auth/callback_activate_user', 'basic-bceid');
-  addLoginPassportUse(discovery, 'oidcBceidActivateDistrictUser', config.get('server:frontend') + '/api/auth/callback_activate_district_user', 'basic-bceid');
+  addLoginPassportUse(
+    discovery,
+    'oidcBceid',
+    config.get('server:frontend') + '/api/auth/callback_bceid',
+    'basic-bceid'
+  );
+  addLoginPassportUse(
+    discovery,
+    'oidcBceidActivateUser',
+    config.get('server:frontend') + '/api/auth/callback_activate_user',
+    'basic-bceid'
+  );
+  addLoginPassportUse(
+    discovery,
+    'oidcBceidActivateDistrictUser',
+    config.get('server:frontend') + '/api/auth/callback_activate_district_user',
+    'basic-bceid'
+  );
   //JWT strategy is used for authorization
-  passport.use('jwt', new JWTStrategy({
-    algorithms: ['RS256'],
-    // Keycloak 7.3.0 no longer automatically supplies matching client_id audience.
-    // If audience checking is needed, check the following SO to update Keycloak first.
-    // Ref: https://stackoverflow.com/a/53627747
-    audience: config.get('server:frontend'),
-    issuer: config.get('tokenGenerate:issuer'),
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: config.get('tokenGenerate:publicKey'),
-    ignoreExpiration: true
-  }, (jwtPayload, done) => {
-    if ((typeof (jwtPayload) === 'undefined') || (jwtPayload === null)) {
-      return done('No JWT token', null);
-    }
+  passport.use(
+    'jwt',
+    new JWTStrategy(
+      {
+        algorithms: ['RS256'],
+        // Keycloak 7.3.0 no longer automatically supplies matching client_id audience.
+        // If audience checking is needed, check the following SO to update Keycloak first.
+        // Ref: https://stackoverflow.com/a/53627747
+        audience: config.get('server:frontend'),
+        issuer: config.get('tokenGenerate:issuer'),
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: config.get('tokenGenerate:publicKey'),
+        ignoreExpiration: true,
+      },
+      (jwtPayload, done) => {
+        if (typeof jwtPayload === 'undefined' || jwtPayload === null) {
+          return done('No JWT token', null);
+        }
 
-    done(null, {
-      email: jwtPayload.email,
-      familyName: jwtPayload.family_name,
-      givenName: jwtPayload.given_name,
-      jwt: jwtPayload,
-      name: jwtPayload.name,
-      user_guid: jwtPayload.user_guid,
-      realmRole: jwtPayload.realm_role
-    });
-  }));
+        done(null, {
+          email: jwtPayload.email,
+          familyName: jwtPayload.family_name,
+          givenName: jwtPayload.given_name,
+          jwt: jwtPayload,
+          name: jwtPayload.name,
+          user_guid: jwtPayload.user_guid,
+          realmRole: jwtPayload.realm_role,
+        });
+      }
+    )
+  );
 });
 //functions for serializing/deserializing users
 passport.serializeUser((user, next) => next(null, user));
 passport.deserializeUser((obj, next) => next(null, obj));
 
-
-app.use(morgan(config.get('server:morganFormat'), { 'stream': logStream }));
+app.use(morgan(config.get('server:morganFormat'), { stream: logStream }));
 //set up routing to auth and main API
 app.use(/(\/api)?/, apiRouter);
 
@@ -154,23 +199,30 @@ apiRouter.use('/auth', authRouter);
 apiRouter.use('/user', userRouter);
 apiRouter.use('/dynamic', dynamicRouter);
 
-
-log.info(`NODE ENV: ${process.env.NODE_ENV} | PORT: ${process.env.API_PORT || 'default-port-8080'}`);
+log.info(
+  `NODE ENV: ${process.env.NODE_ENV || 'NA'} | PORT: ${
+    process.env.API_PORT || 'default-port-8080'
+  }`
+);
 //Handle 500 error
 app.use((err, _req, res, next) => {
   log.error(err?.stack);
-  res?.redirect(config?.get('server:frontend') + '/error?message=500_internal_error');
+  res?.redirect(
+    config?.get('server:frontend') + '/error?message=500_internal_error'
+  );
   next();
 });
 
 // Handle 404 error
 app.use((_req, res) => {
   log.error('404 Error');
-  res.redirect(config?.get('server:frontend') + '/error?message=404_Page_Not_Found');
+  res.redirect(
+    config?.get('server:frontend') + '/error?message=404_Page_Not_Found'
+  );
 });
 
 // Prevent unhandled errors from crashing application
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   log.error('IOSAS| WebAPI | Unhandled Rejection at:', err?.stack || err);
 });
 module.exports = app;
