19bd099a7e8b2af091462e6b0531a6cafef40257
Pushan Mitra <pushan.mitra1@ca.ey.com>
2023-08-28 14:41:31 -0700

    IOSAS | API  |  Add new /environment fix file path add issue

---
+++ backend/src/routes/environment.js
@@ -0,0 +1,41 @@
+'use strict';
+const express = require('express');
+const log = require('../components/logger');
+const config = require('../config/index');
+
+// Express router
+const router = express.Router();
+
+// Environment endpoint
+router.get('/', (req, res) => {
+  const buildInfo = config.get('buildInfo') || { envPrefix: 'na' };
+  log.info('Environment: Route | Env Prefix: ', buildInfo.envPrefix);
+  let envDetails = {};
+  switch (buildInfo.envPrefix) {
+    case 'test':
+      envDetails = {
+        bannerColor: '#b200ff',
+        beceidRegURL:
+          'https://www.test.bceid.ca/register/basic/account_details.aspx?type=regular#action%27',
+      };
+      break;
+    case 'prod':
+      envDetails = {
+        beceidRegURL:
+          'https://www.bceid.ca/register/basic/account_details.aspx?type=regular&eServiceType=basic',
+      };
+      break;
+    default:
+      envDetails = {
+        bannerColor: '#ff0',
+        beceidRegURL:
+          'https://www.development.bceid.ca/register/basic/account_details.aspx?type=regular&eServiceType=basic',
+      };
+  }
+  return res.status(200).json({
+    env: buildInfo.envPrefix,
+    ...envDetails,
+  });
+});
+
+module.exports = router;
