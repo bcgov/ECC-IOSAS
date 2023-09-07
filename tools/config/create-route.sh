c181a23aa17511996509946daba683913915ac23
Pushan Mitra <pushan.mitra1@ca.ey.com>
2023-09-07 14:54:21 -0700

    IOSAS | Remove dry-run

--- tools/config/create-route.sh
+++ tools/config/create-route.sh
@@ -6,12 +6,15 @@ CERTIFICATE=$4
 CA_CERT=$5
 PRIVATE_KEY=$6
 echo Building public route for $NAME in $NAMESPACE
-echo The Host URL is $PUBLIC_HOST
+echo The Host URL is $PUBLIC_HOST || true
+echo Certifcate $CERTIFICATE || true
+echo CA_CERT $CA_CERT || true
+echo PK $PRIVATE_KEY || true
 pwd
 oc --namespace=$NAMESPACE process -f ./openshift/public-route.yml \
  -p NAME=$NAME \
  -p PUBLIC_HOST=$PUBLIC_HOST \
  -p CERTIFICATE="$CERTIFICATE" \
- -p PRIVATE_KEY="$PRIVATE_KEY" | oc --namespace=$NAMESPACE apply -f - --dry-run=client
+ -p PRIVATE_KEY="$PRIVATE_KEY" | oc --namespace=$NAMESPACE apply -f
 
 
