4eb8bfd8778d295b4ea3f97582c3cdeaf0e86417
Pushan Mitra <pushan.mitra1@ca.ey.com>
2023-09-07 14:49:01 -0700

    IOSAS | CI/CD: creating public route using diff template

--- tools/config/create-route.sh
+++ tools/config/create-route.sh
@@ -5,15 +5,13 @@ PUBLIC_HOST=$3
 CERTIFICATE=$4
 CA_CERT=$5
 PRIVATE_KEY=$6
-ROUTE_NAME=$NAME-public
 echo Building public route for $NAME in $NAMESPACE
 echo The Host URL is $PUBLIC_HOST
-oc --namespace=$NAMESPACE delete route $ROUTE_NAME || true && "Route not exists"
-oc --namespace=$NAMESPACE create route edge \
- --service=$NAME \
- --cert="$CERTIFICATE" \
- --key="$PRIVATE_KEY" \
- --ca-cert="$CA_CERT" \
- --hostname=PUBLIC_HOST \
- --insecure-policy=Redirect
+pwd
+oc --namespace=$NAMESPACE process -f ./openshift/public-route.yml \
+ -p NAME=$NAME \
+ -p PUBLIC_HOST=$PUBLIC_HOST \
+ -p CERTIFICATE="$CERTIFICATE" \
+ -p PRIVATE_KEY="$PRIVATE_KEY" | oc --namespace=$NAMESPACE apply -f - --dry-run=client
+
 
