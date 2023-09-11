03dc36663838fb254f8780fd80a3ac6aee95ecae
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2023-09-11 10:50:50 -0700

    DevOps | Add redis to namespace | Injecting all compute related config as pipeline var (#46)

--- backend/src/util/redis/redis-client.js
+++ backend/src/util/redis/redis-client.js
@@ -2,7 +2,6 @@
 let redisClient;
 let connectionClosed = false;
 const Redis = {
-
   /**
    * This method is called during application start and redis client is obtained.
    * The redis client can be reused rather than creating multiple clients.
@@ -11,17 +10,10 @@ const Redis = {
     const IOREDIS = require('ioredis');
     const config = require('../../config');
     const log = require('../../components/logger');
-    if('local' === config.get('environment')){
-      redisClient = new IOREDIS({
-        host: config.get('redis:host'),
-        port: config.get('redis:port')
-      });
-    } else {
-      redisClient = new IOREDIS.Cluster([{
-        host: config.get('redis:host'),
-        port: config.get('redis:port')
-      }]);
-    }
+    redisClient = new IOREDIS({
+      host: config.get('redis:host'),
+      port: config.get('redis:port'),
+    });
     redisClient.on('error', (error) => {
       log.error(`error occurred in redis client. ${error}`);
     });
@@ -41,6 +33,6 @@ const Redis = {
   },
   getRedisClient() {
     return redisClient;
-  }
+  },
 };
 module.exports = Redis;
