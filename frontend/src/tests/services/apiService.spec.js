729d889e4c918a0882c9d0bc96087d85253459e2
mmumby <meredith.mumby@ca.ey.com>
2023-09-16 10:17:04 -0700

    WIP

--- frontend/src/tests/services/apiService.spec.js
+++ frontend/src/tests/services/apiService.spec.js
@@ -1,7 +1,21 @@
-describe('apiService', () => {
-  const name = 'meredith';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import AuthService from '../../services/authService';
+import { AuthRoutes } from '../../utils/constants.js';
 
-  it('first test', () => {
-    expect(name).toEqual('meredith');
+const mockAxios = new MockAdapter(axios);
+
+beforeEach(() => {
+  mockAxios.reset();
+});
+
+describe('AuthService', () => {
+  const mockResponse = { data: { success: true } };
+
+  it('getAuthToken', async () => {
+    mockAxios.onGet(AuthRoutes.TOKEN).reply(200, mockResponse);
+    const response = await AuthService.getAuthToken();
+
+    expect(response.data.success).toBe(true);
   });
 });
