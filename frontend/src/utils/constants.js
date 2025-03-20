515d286d5c5218b864a61d3212e69f8d7e902258
githubmamatha <harmainimamatha@gmail.com>
2025-03-19 20:23:43 -0700

    Cleaned up everything after after Nov 28 commitId 3f5c05b except the Trev changes in Jan.

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,7 +4,7 @@ const dynamicRoot = baseRoot + '/dynamic';
 
 //Authentication endpoints
 export const AuthRoutes = Object.freeze({
-  LOGIN: '/login',
+  LOGIN: authRoot + '/login',
   DASHBOARD: '/',
   LOGIN_BCEID: authRoot + '/logout?loginBceid=true',
   LOGOUT: authRoot + '/logout',
