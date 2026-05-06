598a4cee0b8150daf7b22c4d9fc0d930dc9687e4
githubmamatha <106563495+githubmamatha@users.noreply.github.com>
2026-05-06 11:06:43 -0700

    Fix typo in SchoolInformationTab.vue

--- frontend/src/components/applications/tabs/SchoolInformationTab.vue
+++ frontend/src/components/applications/tabs/SchoolInformationTab.vue
@@ -512,7 +512,7 @@
           <v-label
             >For authorities applying for Group 2 classification, are there
             current plans to seek Group 1 classification in the second or
-            subsequest year(s) of operation?
+            subsequent year(s) of operation?
           </v-label>
           <p>
             {{
